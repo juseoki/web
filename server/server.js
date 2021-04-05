e1a2212f4d120956fb9eba010003348354292514
wing2409 <wing2409@gmail.com>
2021-04-05 17:56:53 +0900

    delete

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const api = require('./routes/index');
 const test = require('./routes/test');
+const db = require('../db/index');
 
 app.use('/api', api);
 app.use('/api', test);
+db();
 
 const port = 3002;
 app.listen(port, ()=>console.log('Listening on port ${port}'));
